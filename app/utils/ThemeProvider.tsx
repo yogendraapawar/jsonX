1b167034367f03fad50637b282e1960f53623e7c
Yogendra Pawar <yogendra.pawar@think360.ai>
2025-01-04 18:05:48 +0530

    typ error ignore

--- app/utils/ThemeProvider.tsx
+++ app/utils/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-
+// @ts-ignore
 export default function ThemeProvider({ children, ...props }) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
